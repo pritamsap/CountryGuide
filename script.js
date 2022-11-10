cfd3fd8bf1bf03b20ba60a640c8dbb6a1beae3ef
PritamSapkota <slicefrost1@gmail.com>
2022-11-10 16:18:41 -0500

    Fetch OpenWeatherMap API, display weather info

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
+let searchBtn = document.getElementById("search-btn");
+console.log(searchBtn);
+
+let weather = {
+    apiKey:"f2f9e74a2b7d5f26102bc588cc9ce27f",
+
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city
+        +  "&units=metric&appid=" + this.apiKey + ""
+        ).then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " +  humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " +  speed  + "km/h ";
+        document.querySelector(".module.two").classList.remove("loading");
+
+    },
+
+    search: function() {
+        this.fetchWeather(countryInp.value);
+    },
+
+};
+
+
 
 let guide = {
     fetchGuide: function(countryName) {
@@ -19,6 +53,8 @@ let guide = {
         document.querySelector(".population").innerText = "Population: " + population;
         document.querySelector(".languages").innerText = "Languages: " + languages;
         document.querySelector(".icon-one").src = img;
+        document.querySelector(".module").classList.remove("loading");
+
 
     },
 
@@ -29,11 +65,30 @@ let guide = {
 };
 
 
+// .catch(() => {
+//     if (countryName.length == 0) {
+//       result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+//     } else {
+//       result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
+//     }
+//   });
+// });
+
+
 countryInp.addEventListener("keyup", (event) => {
     if(event.key == "Enter") {
       guide.search();
+      weather.search();
     }
 });
+
+
+
+searchBtn.addEventListener('click', () => {
+    guide.search();
+    weather.search();
+});
+
 
 
 
